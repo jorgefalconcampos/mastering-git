c593608b56e0477ed3e22aade901d8a8a11a5bd6
Jorge A. Falcón <jorgefalconcampos@gmail.com>
2021-03-07 13:52:17 -0600

    Added function expressions

---
+++ Javascript/functions/function-expressions.js
@@ -0,0 +1,10 @@
+// https://riptutorial.com/javascript/example/843/immediately-invoked-function-expressions
+
+
+let x = function (a, b) {
+    return a + b;
+}
+
+var r = x(20, 4);
+
+console.log(r);
