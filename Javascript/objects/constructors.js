6972f55ee610f272afe1cf4a3076fa3b02422a2e
Jorge A. Falcón <jorgefalconcampos@gmail.com>
2021-03-09 13:39:12 -0600

    Constructor with methods

--- Javascript/objects/constructors.js
+++ Javascript/objects/constructors.js
@@ -4,10 +4,13 @@ function Person(name, last_name, email){
     this.name = name;
     this.last_name = last_name;
     this.email = email;
+    this.fullName = function(){
+        return this.name + ' ' + this.last_name;
+    }
 }
 
 let p = new Person('Luis', 'Lopez', 'll@mail.com');
-console.log(p);
+console.log(p.fullName());
 
 let m = new Person('Maria', 'Magdalena', 'mm@gmail.com');
-console.log(m);
+console.log(m.fullName());
