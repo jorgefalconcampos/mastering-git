0a1343f4c8c6fed9fb76449b4d7c06dc57a236bf
Jorge Antonio Falcón Campos <jorgef_campos@outlook.com>
2021-03-15 11:31:29 -0600

    try-catch-finally in js

---
+++ Javascript/errors/try-catch.js
@@ -0,0 +1,11 @@
+"use strict";
+
+
+try{
+    // since we're using strict mode, the x variable statement will result on an error
+    x = 10;
+}
+catch(err){
+    console.log(error);
+    // console.error(err);
+}
