bc96a13e26cb06a14ee3b02b8808019a4f1f248e
Jorge Antonio Falcón Campos <jorgef_campos@outlook.com>
2021-03-15 20:03:39 -0600

    Arrow functions basics

--- Javascript/functions/arrow-functions/arrowFunctions.js
+++ Javascript/functions/arrow-functions/arrowFunctions.js
@@ -1,3 +1,16 @@
 let arrowFunction = () => {
     console.log("Arrow function");
 }
+
+// const greeting = () => {
+//     return 'Hello from arrow function';
+// }
+
+const greeting = () => 'Hello from arrow function';
+
+console.log(greeting());
+
+const greetingWithParams = (msg) => console.log(`The message is ${msg}`);
+
+// if only 1 parameter is received, we can ommit the parentheses
+const greetingWithParams2 = msg => console.log(`The message is ${msg}`);
